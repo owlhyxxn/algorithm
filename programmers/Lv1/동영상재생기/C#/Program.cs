c9669e80fdb92ba724819722754a0232f4a871db
hyeonjong.kim <owl.hyxxn@gmail.com>
2024-12-29 22:48:58 +0900

    algorithm study

--- programmers/Lv1/동영상재생기/C#/Program.cs
+++ programmers/Lv1/동영상재생기/C#/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿# PCCP
+using System;
 
 public class Solution
 {
---
+++ programmers/Lv1/지폐접기/python/init.py
@@ -0,0 +1,14 @@
+def solution(wallet, bill):
+    answer = 0
+    largeBill = max(bill[0], bill[1])
+    smallBill = min(bill[0], bill[1])
+    largeWallet = max(wallet[0], wallet[1])
+    smallWallet = min(wallet[0], wallet[1])
+    
+    while (largeBill > largeWallet or smallBill > smallWallet):
+        largeBillBefore = int(largeBill / 2)
+        smallBillBefore = smallBill
+        largeBill = max(largeBillBefore, smallBillBefore)
+        smallBill = min(largeBillBefore, smallBillBefore)
+        answer += 1
+    return answer
